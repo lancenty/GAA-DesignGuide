984644cc02093709317393a5ba083cb57ffc817b
Bob Ryan <rryan14@gmail.com>
2018-08-09 21:56:19 -0400

    Removed unneeded file, fixed href

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -61,7 +61,7 @@ const Sidebar = ({ ...props }) => {
   );
   var brand = (
     <div className={classes.logo}>
-      <a href="https://www.creative-tim.com" className={classes.logoLink}>
+      <a href="/introduction" className={classes.logoLink}>
         <div className={classes.logoImage}>
           <img src={logo} alt="logo" className={classes.img} />
         </div>
